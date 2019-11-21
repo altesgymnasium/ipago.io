8fbe167c7ebd76f7e23815f8e4b197633bf8294a
QuickWrite <playk3603@gmail.com>
2019-11-21 18:15:49 +0100

    Initialize

---
+++ website/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="de-de">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hauptseite | ipago.io</title>
+
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <!--[if lt IE 7]><p>Du nutzt einen <strong>veralteten</strong> Browser. Bitte <a href="http://browsehappy.com/">nutze einen neueren Browser</a> um deine Erfahrung zu verbessern</p><![endif]-->
+  
+</body>
+</html>
